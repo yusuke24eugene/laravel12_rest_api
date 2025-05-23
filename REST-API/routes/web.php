dd6abea73d8cdbecff200fc5ad96b724a7a6fd5f
PepeSmith <pepe.smith@email.com>
2025-05-23 22:42:38 +0800

    Remove EnsureFrontendRequestsAreStateful::class in web.php

--- REST-API/routes/web.php
+++ REST-API/routes/web.php
@@ -4,29 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
-use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 Route::get('/', function () {
 return view('welcome');
 });
 
 
-Route::middleware([
-    EnsureFrontendRequestsAreStateful::class,
-    'web',
-])->group(function () {
-    // Register & login (public)
+// Register & login (public)
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 
-});
-
 // Protected routes
-Route::middleware([
-    'auth:sanctum',
-    EnsureFrontendRequestsAreStateful::class,
-    'web',
-])->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [UserController::class, 'logout']);
     Route::get('/user', function (Request $request) {
         return $request->user();
