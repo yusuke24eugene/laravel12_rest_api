04bbaf82d3ac1f6b00f3ad0fd0c8a6991af9505b
PepeSmith <pepe.smith@email.com>
2025-05-23 21:13:05 +0800

    Add EnsureFrontendRequestsAreStateful::class in middleware

--- REST-API/routes/web.php
+++ REST-API/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 Route::get('/', function () {
 return view('welcome');
@@ -14,7 +15,11 @@ Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 
 // Protected routes
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware([
+    'auth:sanctum',
+    EnsureFrontendRequestsAreStateful::class,
+    'web',
+])->group(function () {
     Route::post('/logout', [UserController::class, 'logout']);
     Route::get('/user', function (Request $request) {
         return $request->user();
--- docker/start.sh
+++ docker/start.sh
@@ -5,7 +5,6 @@ php artisan config:clear
 php artisan view:clear
 php artisan config:clear
 php artisan config:cache
-php artisan session:table
 php artisan migrate --force
 
 # Start PHP-FPM
